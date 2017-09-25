a3b26c601613ecada44b64901581a4c74c303260
ajking98 <agedi98@gmail.com>
2017-09-24 21:24:11 -0400

    first commit

---
+++ src/MinHeap.java
@@ -0,0 +1,60 @@
+/**
+ * Your implementation of a min heap.
+ *
+ * @author YOUR NAME HERE
+ * @userid YOUR USER ID HERE (i.e. gburdell3)
+ * @GTID YOUR GT ID HERE (i.e. 900000000)
+ * @version 1.0
+ */
+public class MinHeap<T extends Comparable<? super T>>
+    implements HeapInterface<T> {
+
+    private T[] backingArray;
+    private int size;
+    // Do not add any more instance variables. Do not change the declaration
+    // of the instance variables above.
+
+    /**
+     * Creates a Heap with an initial capacity of {@code INITIAL_CAPACITY}
+     * for the backing array.
+     *
+     * Use the constant field in the interface. Do not use magic numbers!
+     */
+    public MinHeap() {
+
+    }
+
+    @Override
+    public void add(T item) {
+
+    }
+
+    @Override
+    public T remove() {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // DO NOT MODIFY THIS METHOD!
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        // DO NOT MODIFY THIS METHOD!
+        return size;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Comparable[] getBackingArray() {
+        // DO NOT MODIFY THIS METHOD!
+        return backingArray;
+    }
+
+}
